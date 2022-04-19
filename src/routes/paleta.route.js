8517fde2a9a801897b0fe1242433e37364f46ef0
Leandro Spitzer Godoi <leandro_spitzer@hotmail.com>
2022-04-19 20:29:30 -0300

    ElGeladonServer

---
+++ src/routes/paleta.route.js
@@ -0,0 +1,12 @@
+const route = require('express').Router();
+const controllerPaletas = require('../controllers/paleta.controller');
+
+
+route.get('/find-paletas', controllerPaletas.findAllPaletasController);
+route.get('/paleta/:id', controllerPaletas.findByIdPaletasController);
+route.post('/create', controllerPaletas.createPaletaController);
+route.put('/update/:id', controllerPaletas.updatePaletaController);
+route.delete('/delete/:id', controllerPaletas.deletePaletaController);
+
+
+module.exports = route;
---
+++ src/services/paleta.service.js
@@ -0,0 +1,68 @@
+const paletas = [
+  {
+    id: 1,
+    sabor: 'Açaí com Leite Condensado',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: './assets/IMG/acai-com-leite-condensado.png',
+    preco: 10.0,
+  },
+  {
+    id: 2,
+    sabor: 'Banana com Nutella',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: './assets/IMG/banana-com-nutella.png',
+    preco: 10.0,
+  },
+  {
+    id: 3,
+    sabor: 'Chocolate Belga',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: './assets/IMG/chocolate-belga.png',
+    preco: 7.0,
+  },
+  {
+    id: 4,
+    sabor: 'Limão',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: './assets/IMG/limao.png',
+    preco: 9.0,
+  },
+];
+
+const findAllPaletasService = () => {
+  return paletas;
+};
+const findByIdPaletasService = (idParam) => {
+  return (paleta = paletas.find((paleta) => paleta.id === idParam));
+};
+
+const createPaletaService = (newPaleta) => {
+  const newId = paletas.length + 1;
+  newPaleta.id = newId;
+  paletas.push(newPaleta);
+  return newPaleta;
+};
+
+const updatePaletaService = (id, paletaEdited) => {
+  paletaEdited['id'] = id;
+  const paletaIndex = paletas.findIndex((paleta) => paleta.id == id);
+  paletas[paletaIndex] = paletaEdited;
+  return paletaEdited;
+};
+
+const deletePaletaService = (id) => {
+  const paletaIndex = paletas.findIndex((paleta) => paleta.id == id);
+  return paletas.splice(paletaIndex, 1);
+};
+
+module.exports = {
+  findAllPaletasService,
+  findByIdPaletasService,
+  createPaletaService,
+  updatePaletaService,
+  deletePaletaService,
+};
